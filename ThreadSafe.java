95a9b7329bec7ed44397956a107d6f484fa571aa
Prashant Yadav <46739132+prashantyadav4686@users.noreply.github.com>
2019-02-04 09:18:49 +0530

    Update ThreadSafe.java

--- ThreadSafe.java
+++ ThreadSafe.java
@@ -20,7 +20,7 @@
 		ThreadSafe b = ThreadSafe.getInstance();
 		
 		System.out.println("Object1: " + a.str);
-		b.str = "Shivam Agarwal";
+		b.str = "Prashant Yadav";
 		System.out.println("After Changes:");
 		System.out.println("Object1: " + a.str);
 		System.out.println("Object2: " + b.str);
